a138db64fce3b5bddb35153532aa7ab0b5f30d69
Diogo de Aguiar <diogobarreto2004@gmail.com>
2026-03-22 20:28:23 -0300

    feat: added theme, title and home screen to the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-
 import 'data/models/customer_model.dart';
 import 'ui/screens/home_screen.dart';
 
 void main() async {
-
   WidgetsFlutterBinding.ensureInitialized();
   
   await Hive.initFlutter();
-  
   Hive.registerAdapter(CustomerModelAdapter());
   
   runApp(
@@ -26,11 +23,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Customer Registration',
+      title: 'Customer CRUD',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-        useMaterial3: true, 
+        useMaterial3: true,
       ),
       home: const HomeScreen(),
     );
